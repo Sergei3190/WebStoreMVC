967259eed33b9ea4eb4d8563f7eaa4938a6120b4
Сергей Лысков <sergkosya@mail.ru>
2023-01-13 08:09:44 +0300

    add greetings

---
+++ WebStoreMVC/Contollers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStoreMVC.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();    
+        }
+    }
+}
