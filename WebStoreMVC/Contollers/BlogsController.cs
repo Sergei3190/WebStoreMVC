aad351b7a1642c8b4c57635d13cdbe1d90d4d325
Сергей Лысков <sergkosya@mail.ru>
2023-03-27 21:18:24 +0300

    add blogs controller

---
+++ WebStoreMVC/Contollers/BlogsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+//TODO
+public class BlogsController : Controller
+{
+    public IActionResult Index() => View(); //должен вернуть представление списка блогов blog.html
+    public IActionResult ShopBlog() => View(); //должен вернуть представление блога магазина  blog-single.html
+}
