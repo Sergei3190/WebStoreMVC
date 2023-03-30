7205620038e170f6dcf746861844ef64e1325c6d
Сергей Лысков <sergkosya@mail.ru>
2023-03-30 17:03:57 +0300

    create view model for Employee

---
+++ WebStoreMVC/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStoreMVC.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public int Age { get; set; }
+    }
+}
