0f191bc2a5f378faa078b32d1d0e5f029789273c
Сергей Лысков <sergkosya@mail.ru>
2023-03-30 17:03:15 +0300

    pushing test data into a separate class

---
+++ WebStoreMVC/Data/TestData.cs
@@ -0,0 +1,13 @@
+﻿using WebStoreMVC.Models;
+
+namespace WebStoreMVC.Data;
+
+public static class TestData
+{
+    public static ICollection<Employee> Employees { get; } = new List<Employee>()
+    {
+        new () { Id = 1, Age = 23, LastName = "Иванов", FirstName = "Иван", MiddleName = "Иванович" },
+        new () { Id = 2, Age = 27, LastName = "Петров", FirstName = "Петр", MiddleName = "Петрович" },
+        new () { Id = 3, Age = 18, LastName = "Сидоров", FirstName = "Сидор", MiddleName = "Сидорович" }
+    };
+}
