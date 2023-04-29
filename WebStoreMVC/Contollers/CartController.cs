09c61112c8e9c4e7d8f91d048286a6e23e17e586
Сергей Лысков <sergkosya@mail.ru>
2023-04-29 19:46:38 +0300

    added visual part of the cart + links in other views

--- WebStoreMVC/Contollers/CartController.cs
+++ WebStoreMVC/Contollers/CartController.cs
@@ -1,7 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using WebStoreMVC.Services.Interfaces;
+
 namespace WebStoreMVC.Controllers;
 public class CartController : Controller
 {
-    public IActionResult Index() => View();
+
+    private readonly ICartService _service;
+
+    public CartController(ICartService service)
+    {
+        _service = service;
+    }
+    public IActionResult Index() => View(_service.GetCartViewModel());
+
+    public IActionResult Add(int id)
+    {
+        _service.Add(id);
+        return RedirectToAction(nameof(Index), "Cart");
+    }
+
+    public IActionResult Decrement(int id)
+    {
+        _service.Decrement(id);
+        return RedirectToAction(nameof(Index), "Cart");
+    }
+
+    public IActionResult Remove(int id)
+    {
+        _service.Remove(id);
+        return RedirectToAction(nameof(Index), "Cart");
+    }
 }
