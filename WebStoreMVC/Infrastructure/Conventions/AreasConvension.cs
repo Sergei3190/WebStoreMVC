471d44456ff553319a328200d94014618156a9d6
Сергей Лысков <sergkosya@mail.ru>
2023-05-11 14:08:19 +0400

    added areas conversion

---
+++ WebStoreMVC/Infrastructure/Conventions/AreasConvension.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace WebStoreMVC.Infrastructure.Conventions;
+
+public class AreasConvension : IControllerModelConvention
+{
+    public void Apply(ControllerModel controller)
+    {
+        var typeNamespace = controller.ControllerType.Namespace;
+
+        if (string.IsNullOrEmpty(typeNamespace)) return;
+
+        const string areasNamespaceSuffix = "Areas.";
+        const int areasNamespaceSuffixLength = 6;
+
+        var areasIndex = typeNamespace.IndexOf(areasNamespaceSuffix, StringComparison.OrdinalIgnoreCase);   
+
+        if(areasIndex < 0) return;
+
+        areasIndex += areasNamespaceSuffixLength;
+
+        var areaName = typeNamespace[areasIndex..typeNamespace.IndexOf('.', areasIndex)]; 
+
+        if (string.IsNullOrEmpty(areaName)) return;
+
+        if (controller.Attributes.OfType<AreaAttribute>().Any(a => a.RouteKey == "area" && a.RouteValue == areaName)) return;
+
+        controller.RouteValues["area"] = areaName;
+    }
+}
