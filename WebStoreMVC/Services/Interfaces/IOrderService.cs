7379514c9fa9e2bced7e44dacc78430f32029af8
Сергей Лысков <sergkosya@mail.ru>
2023-05-11 14:11:22 +0400

    added order service

---
+++ WebStoreMVC/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using WebStoreMVC.Domain.Entities.Orders;
+using WebStoreMVC.ViewModels;
+
+namespace WebStoreMVC.Services.Interfaces;
+public interface IOrderService
+{
+    Task<IEnumerable<Order>> GetUserOrdersAsync(string userName, CancellationToken cancel = default);
+
+    Task<Order?> GetOrderByIdAsync(int id, CancellationToken cancel = default);
+
+    Task<Order> CreateOrderAsync(string userName, CartViewModel cart, OrderViewModel order, CancellationToken cancel = default);
+}
