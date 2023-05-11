68242c6122ccc6afd4823cf87b469e0ee39f5361
Сергей Лысков <sergkosya@mail.ru>
2023-05-11 14:10:43 +0400

    added user profile

---
+++ WebStoreMVC/Contollers/UserProfileController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStoreMVC.Services.Interfaces;
+using WebStoreMVC.ViewModels;
+
+namespace WebStoreMVC.Controllers;
+
+[Authorize]
+public class UserProfileController : Controller
+{
+	private readonly IProductsService _service;
+	private readonly IMapper _mapper;
+
+	public UserProfileController(IProductsService service, IMapper mapper)
+	{
+		_service = service;
+		_mapper = mapper;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+    public async Task<IActionResult> Orders([FromServices] IOrderService service)
+    {
+		var orders = await service.GetUserOrdersAsync(User!.Identity!.Name!);
+
+        return View(orders.Select(o => new UserOrderViewModel()
+		{
+			Id = o.Id,
+			Date = o.Date,
+			Address = o.Address,
+			Phone = o.Phone,
+			Description = o.Description,	
+			TotalPrice = o.TotalPrice
+		}));
+    }
+}
