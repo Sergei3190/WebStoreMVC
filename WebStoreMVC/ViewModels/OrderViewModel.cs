1c1898de0e98679ee31af106d4899b523f0e2b89
Сергей Лысков <sergkosya@mail.ru>
2023-05-11 14:11:55 +0400

    added order view models

---
+++ WebStoreMVC/ViewModels/OrderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStoreMVC.ViewModels;
+
+public class OrderViewModel
+{
+	public OrderViewModel()
+	{
+		Address = null!;
+		Phone = null!;
+	}
+
+	[Required(ErrorMessage = "Не заполнен адрес заказа"), MaxLength(200)]
+	[Display(Name = "Адрес заказа")]
+	public string Address { get; set; }
+
+	[Required(ErrorMessage = "Не заполнен телефон"), MaxLength(200)]
+	[Display(Name = "Телефон")]
+	[DataType(DataType.PhoneNumber)]
+	public string Phone { get; set; }
+
+	[MaxLength(200)]
+	[Display(Name = "Комментарий")]
+	[DataType(DataType.MultilineText)]
+	public string? Description { get; set; }
+}
---
+++ WebStoreMVC/ViewModels/UserOrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebStoreMVC.ViewModels;
+
+public class UserOrderViewModel
+{
+    public UserOrderViewModel()
+    {
+        Address = null!;
+        Phone = null!;
+    }
+
+    public int Id { get; set; }
+
+    public DateTimeOffset Date { get; set; }
+
+    public string Address { get; set; }
+
+    public string Phone { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal TotalPrice { get; set; }
+}
