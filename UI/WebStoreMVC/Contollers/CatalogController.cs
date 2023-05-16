8736f3255cf7760936f900587f0b608aa49ba412
Сергей Лысков <sergkosya@mail.ru>
2023-05-16 16:48:10 +0400

    refactoring, transfer mappers and InCookiesCartService to main project

--- UI/WebStoreMVC/Contollers/CatalogController.cs
+++ UI/WebStoreMVC/Contollers/CatalogController.cs
@@ -3,43 +3,43 @@
 using Microsoft.AspNetCore.Mvc;
 
 using WebStoreMVC.Domain.Entities;
+using WebStoreMVC.Infrastructure.Mappings;
 using WebStoreMVC.Interfaces.Services;
-using WebStoreMVC.Services.Mappers.Mappings;
 using WebStoreMVC.ViewModels;
 
 namespace WebStoreMVC.Contollers;
 public class CatalogController : Controller
 {
-    private readonly IProductsService _service;
-    private readonly IMapper _mapper;
+	private readonly IProductsService _service;
+	private readonly IMapper _mapper;
 
-    public CatalogController(IProductsService service, IMapper mapper)
-    {
-        _service = service;
-        _mapper = mapper;
-    }
+	public CatalogController(IProductsService service, IMapper mapper)
+	{
+		_service = service;
+		_mapper = mapper;
+	}
 
-    public IActionResult Index([Bind("SectionId", "BrandId")] ProductFilter filter)
-    {
-        var products = _service.GetProducts(filter);
+	public IActionResult Index([Bind("SectionId", "BrandId")] ProductFilter filter)
+	{
+		var products = _service.GetProducts(filter);
 
-        return View(new CatalogViewModel()
-        {
-            SectionId = filter.SectionId,
-            BrandId = filter.BrandId,
-            Products = products
-                .OrderBy(p => p.Order)
-                .Select(p => _mapper.Map<ProductViewModel>(p))
-        });
-    }
+		return View(new CatalogViewModel()
+		{
+			SectionId = filter.SectionId,
+			BrandId = filter.BrandId,
+			Products = products
+				.OrderBy(p => p.Order)
+				.Select(p => _mapper.Map<ProductViewModel>(p))
+		});
+	}
 
-    public async Task<IActionResult> Details(int id)
-    {
-        var product = await _service.GetProductById(id).ConfigureAwait(false);
+	public async Task<IActionResult> Details(int id)
+	{
+		var product = await _service.GetProductById(id).ConfigureAwait(false);
 
-        if (product is null)
-            return NotFound();
+		if (product is null)
+			return NotFound();
 
-        return View(product.ToView());
-    }
+		return View(product.ToView());
+	}
 }
--- UI/WebStoreMVC/Contollers/WebApiController.cs
+++ UI/WebStoreMVC/Contollers/WebApiController.cs
@@ -2,21 +2,21 @@
 
 using WebStoreMVC.Interfaces.TestApi;
 
-namespace WebStoreMVC.Controllers
+namespace WebStoreMVC.Contollers
 {
-    public class WebApiController : Controller
-    {
-        private readonly IValueService _valueService;
+	public class WebApiController : Controller
+	{
+		private readonly IValueService _valueService;
 
-        public WebApiController(IValueService valueService)
-        {
-            _valueService = valueService;
-        }
+		public WebApiController(IValueService valueService)
+		{
+			_valueService = valueService;
+		}
 
-        public IActionResult Index()
-        {
-            var values = _valueService.GetValues();
-            return View(values);
-        }
-    }
+		public IActionResult Index()
+		{
+			var values = _valueService.GetValues();
+			return View(values);
+		}
+	}
 }
