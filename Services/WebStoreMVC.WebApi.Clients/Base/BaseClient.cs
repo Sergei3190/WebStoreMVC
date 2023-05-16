1e381ec37f8da9bdbf262c3d7420fe539f621452
Сергей Лысков <sergkosya@mail.ru>
2023-05-16 18:56:49 +0400

    added http methods in BaseClients

--- Services/WebStoreMVC.WebApi.Clients/Base/BaseClient.cs
+++ Services/WebStoreMVC.WebApi.Clients/Base/BaseClient.cs
@@ -1,4 +1,7 @@
-﻿namespace WebStoreMVC.WebApi.Clients.Base;
+﻿using System.Net.Http.Json;
+using System.Net;
+
+namespace WebStoreMVC.WebApi.Clients.Base;
 
 public abstract class BaseClient
 {
@@ -10,4 +13,48 @@ public abstract class BaseClient
         HttpClient = httpClient;
         Address = address;
     }
+
+	protected T? Get<T>(string url) => GetAsync<T>(url).Result;
+
+	protected async Task<T?> GetAsync<T>(string url, CancellationToken cancel = default)
+	{
+		var response = await HttpClient.GetAsync(url, cancel).ConfigureAwait(false);
+
+		switch (response.StatusCode)
+		{
+			case HttpStatusCode.NoContent:
+			case HttpStatusCode.NotFound:
+				return default(T?);
+			default:
+				var result = await response
+					.Content
+					.ReadFromJsonAsync<T?>(cancellationToken: cancel)
+					.ConfigureAwait(false);
+				return result;
+		}
+	}
+
+	protected HttpResponseMessage Post<T>(string url, T value) => PostAsync<T>(url, value).Result!;
+
+	protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value, CancellationToken cancel = default)
+	{
+		var response = await HttpClient.PostAsJsonAsync(url, value, cancel).ConfigureAwait(false);
+		return response.EnsureSuccessStatusCode();
+	}
+
+	protected HttpResponseMessage Put<T>(string url, T value) => PutAsync<T>(url, value).Result!;
+
+	protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value, CancellationToken cancel = default)
+	{
+		var response = await HttpClient.PutAsJsonAsync(url, value, cancel).ConfigureAwait(false);
+		return response.EnsureSuccessStatusCode();
+	}
+
+	protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result!;
+
+	protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken cancel = default)
+	{
+		var response = await HttpClient.DeleteAsync(url, cancel).ConfigureAwait(false);
+		return response;
+	}
 }
