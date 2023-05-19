92ef9a48ad95c201d359155c81c39528f82be807
Сергей Лысков <sergkosya@mail.ru>
2023-05-19 20:19:36 +0400

    creating a centralized web api address system

---
+++ Services/WebStoreMVC.Interfaces/WebApiAddresses.cs
@@ -0,0 +1,19 @@
+﻿namespace WebStoreMVC.Interfaces;
+
+public static class WebApiAddresses
+{
+	public static class V1
+	{
+		public const string Employees = "api/v1/employees";
+		public const string Orders = "api/v1/orders";
+		public const string Products = "api/v1/products";
+		public const string Blogs = "api/v1/blogs";
+		public const string Values = "api/v1/values";
+
+		public static class Identity
+		{
+			public const string Users = "api/v1/users";
+			public const string Roles = "api/v1/roles";
+		}
+	}
+}
