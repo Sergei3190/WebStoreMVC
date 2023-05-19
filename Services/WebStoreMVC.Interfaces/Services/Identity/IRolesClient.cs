e2d497ec5f7b74660200421d71bbcfbef54cd642
Сергей Лысков <sergkosya@mail.ru>
2023-05-19 20:35:24 +0400

    added interfaces for Identity web api

---
+++ Services/WebStoreMVC.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using WebStoreMVC.Domain.Entities.Identity;
+
+namespace WebStoreMVC.Interfaces.Services.Identity;
+
+public interface IRolesClient : IRoleStore<Role>
+{
+
+}
---
+++ Services/WebStoreMVC.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using WebStoreMVC.Domain.Entities.Identity;
+
+namespace WebStoreMVC.Interfaces.Services.Identity;
+
+public interface IUsersClient :
+	IUserStore<User>,
+	IUserRoleStore<User>,
+	IUserPasswordStore<User>,
+	IUserEmailStore<User>,
+	IUserPhoneNumberStore<User>,
+	IUserTwoFactorStore<User>,
+	IUserLoginStore<User>,
+	IUserClaimStore<User>
+{
+}
