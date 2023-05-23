de98b27b93de276e861f72ffd81fd96f4555b7b7
Сергей Лысков <sergkosya@mail.ru>
2023-05-23 19:16:01 +0400

    added a library of logging

---
+++ Common/WebStoreMVC.Logging/Log4Net/Log4NetLogger.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Xml;
+
+using log4net;
+
+using Microsoft.Extensions.Logging;
+
+namespace WebStoreMVC.Logging.Log4Net;
+
+public class Log4NetLogger : ILogger
+{
+    private readonly ILog _log;
+
+    public Log4NetLogger(string сategory, XmlElement сonfiguration)
+    {
+        var loggerRepository = LogManager
+           .CreateRepository(
+                Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+        _log = LogManager.GetLogger(loggerRepository.Name, сategory);
+
+        log4net.Config.XmlConfigurator.Configure(сonfiguration);
+    }
+
+    public IDisposable BeginScope<TState>(TState state) => null!;
+
+    public bool IsEnabled(LogLevel level) => level switch
+    {
+        LogLevel.None => false,
+        LogLevel.Trace => _log.IsDebugEnabled,
+        LogLevel.Debug => _log.IsDebugEnabled,
+        LogLevel.Information => _log.IsInfoEnabled,
+        LogLevel.Warning => _log.IsWarnEnabled,
+        LogLevel.Error => _log.IsErrorEnabled,
+        LogLevel.Critical => _log.IsFatalEnabled,
+        _ => throw new InvalidEnumArgumentException(nameof(level), (int)level, typeof(LogLevel))
+    };
+
+    public void Log<TState>(
+        LogLevel level,
+        EventId id,
+        TState state,
+        Exception? error,
+        Func<TState, Exception?, string> formatter)
+    {
+        if (formatter is null) throw new ArgumentNullException(nameof(formatter));
+
+        if (!IsEnabled(level))
+            return;
+
+        var log_string = formatter(state, error);
+        if (string.IsNullOrWhiteSpace(log_string) && error is null)
+            return;
+
+        switch (level)
+        {
+            default: throw new InvalidEnumArgumentException(nameof(level), (int)level, typeof(LogLevel));
+
+            case LogLevel.None: break;
+
+            case LogLevel.Trace:
+            case LogLevel.Debug:
+                _log.Debug(log_string);
+                break;
+
+            case LogLevel.Information:
+                _log.Info(log_string);
+                break;
+
+            case LogLevel.Warning:
+                _log.Warn(log_string);
+                break;
+
+            case LogLevel.Error:
+                _log.Error(log_string, error);
+                break;
+
+            case LogLevel.Critical:
+                _log.Fatal(log_string, error);
+                break;
+        }
+    }
+}
---
+++ Common/WebStoreMVC.Logging/Log4Net/Log4NetLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using System.Xml;
+
+using Microsoft.Extensions.Logging;
+
+namespace WebStoreMVC.Logging.Log4Net;
+
+public class Log4NetLoggerProvider : ILoggerProvider
+{
+    private readonly string _configurationFile;
+    private readonly ConcurrentDictionary<string, Log4NetLogger> _loggers = new();
+
+    public Log4NetLoggerProvider(string сonfigurationFile) => _configurationFile = сonfigurationFile;
+
+    public ILogger CreateLogger(string categoryName) => _loggers.GetOrAdd(
+        categoryName,
+        (categoryName, config) =>
+        {
+            var xml = new XmlDocument();
+            xml.Load(config);
+            return new Log4NetLogger(categoryName, xml["log4net"]
+                ?? throw new InvalidOperationException("Не удалось извлечь из xml-документа элемент log4net"));
+        },
+        _configurationFile);
+
+    public void Dispose() => _loggers.Clear();
+}
