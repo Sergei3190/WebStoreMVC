7735aff5ffedf82a017e5140401b7f9cf8e51abd
Сергей Лысков <sergkosya@mail.ru>
2023-05-31 13:46:52 +0400

    added cart component

---
+++ UI/WebStoreMVC/Components/CartViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebStoreMVC.Interfaces.Services.Applied;
+
+namespace WebStoreMVC.Components;
+public class CartViewComponent : ViewComponent
+{
+	private readonly ICartStore _CartStore;
+
+	public CartViewComponent(ICartStore CartStore) => _CartStore = CartStore;
+
+	public IViewComponentResult Invoke()
+	{
+		ViewBag.Count = _CartStore.Cart.ItemsCount;
+		return View();
+	}
+}
